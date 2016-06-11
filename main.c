130a60149f849b7e533b74a9422df2cfe9761a4c
Ander Juaristi <a@juaristi.eus>
2016-06-11 17:44:10 +0200

    Final integration completed
     * main.c (main): added option '-s'. Factor capture logic out.
       (run): new static method. Takes a picture from the camera.
       (create_unique_filename, write_to_disk): new static methods.
       (sighandler): do not capture SIGKILL, since it's uncatchable.

--- main.c
+++ main.c
@@ -11,6 +11,7 @@
 #include <string.h>
 #include <unistd.h>
 #include <linux/videodev2.h>
+#include <sys/stat.h>
 #include "utils.h"
 #include "appbase.h"
 #include "uvc.h"
@@ -21,19 +22,54 @@ int stop;
 #define SHOULD_STOP(v) (stop = v)
 #define IS_STOPPED()   (stop)
 
-void print_usage_and_exit(const char *name)
+static char *create_unique_filename()
+{
+	struct stat s;
+#define COUNT_LIMIT 9
+	unsigned int count = 0;
+	char filename_template[] = "picture";
+	size_t size = sizeof(filename_template) + 2;
+	char *filename = ec_malloc(size);
+
+	do {
+		snprintf(filename, size, "%s.%d", filename_template, count++);
+	} while (stat(filename, &s) >= 0 && count <= COUNT_LIMIT);
+
+	if (count > COUNT_LIMIT)
+		snprintf(filename, size, "%s.%d", filename_template, 0);
+
+	return filename;
+#undef COUNT_LIMIT
+}
+
+static void write_to_disk(const char *data, size_t size)
+{
+	FILE *f;
+	char *filename = create_unique_filename();
+
+	f = fopen(filename, "w");
+	if (f) {
+		fwrite(data, size, 1, f);
+		fclose(f);
+	}
+
+	free(filename);
+}
+
+static void print_usage_and_exit(const char *name)
 {
 	if (name) {
 		printf("Usage: %s [OPTIONS] <app name> <username> <password>\n"
 				"Options:\n"
 				"    -w secs        Sleep this amount of seconds between shots\n"
-				"    -d             Display debug messages\n",
+				"    -d             Display debug messages\n"
+				"    -s             Take one single shot and exit\n",
 				name);
 	}
 	exit(1);
 }
 
-void sighandler(int s)
+static void sighandler(int s)
 {
 	char *signame;
 
@@ -43,9 +79,6 @@ void sighandler(int s)
 		break;
 	case SIGQUIT:
 		signame = "SIGQUIT";
-		break;
-	case SIGKILL:
-		signame = "SIGKILL";
 		break;
 	case SIGTERM:
 		signame = "SIGTERM";
@@ -70,20 +103,37 @@ void sighandler(int s)
 	SHOULD_STOP(1);
 }
 
+static void run(struct camera *camera, struct appbase *ab, bool debug)
+{
+	if (uvc_capture_frame(camera)) {
+			if (!appbase_push_frame(ab,
+					camera->frame, camera->frame_bytes_used,
+					&camera->capture_time))
+				fprintf(stderr, "ERROR: Could not send frame\n");
+
+		if (debug)
+			write_to_disk(camera->frame, camera->frame_bytes_used);
+
+		memset(camera->frame, 0, camera->frame_size);
+		camera->frame_bytes_used = 0;
+	} else {
+		fprintf(stderr, "ERROR: Could not capture frame\n");
+	}
+
+}
+
 int main(int argc, char **argv)
 {
 	int opt;
 	char *endptr;
 	long int wait_time = DEFAULT_WAIT_TIME;
-	int debug = 0;
-	char *app_name, *username, *password;
+	bool debug = false, oneshot = false;
 	struct sigaction sig;
 	struct appbase *ab;
 	struct camera *camera;
-	struct frame *frame;
 
 	/* Parse command-line options */
-	while ((opt = getopt(argc, argv, "w:d")) != -1) {
+	while ((opt = getopt(argc, argv, "w:ds")) != -1) {
 		switch (opt) {
 		case 'w':
 			wait_time = strtol(optarg, &endptr, 10);
@@ -91,7 +141,10 @@ int main(int argc, char **argv)
 				print_usage_and_exit(argv[0]);
 			break;
 		case 'd':
-			debug = 1;
+			debug = true;
+			break;
+		case 's':
+			oneshot = true;
 			break;
 		default:
 			print_usage_and_exit(argv[0]);
@@ -110,7 +163,6 @@ int main(int argc, char **argv)
 
 	sigaction(SIGINT, &sig, NULL);
 	sigaction(SIGQUIT, &sig, NULL);
-	sigaction(SIGKILL, &sig, NULL);
 	sigaction(SIGTERM, &sig, NULL);
 	sigaction(SIGABRT, &sig, NULL);
 	sigaction(SIGTRAP, &sig, NULL);
@@ -152,21 +204,28 @@ int main(int argc, char **argv)
 	 * | Y (pixel 0) | U (both) | Y (pixel 1) | V (both) |
 	 * +-------------+----------+-------------+----------+
 	 */
-	if (!uvc_alloc_frame(camera, V4L2_PIX_FMT_YUYV, 320, 240))
+	camera->width = 320;
+	camera->height = 240;
+	camera->format = V4L2_PIX_FMT_YUYV;
+	if (!uvc_alloc_frame(camera))
 		fatal("Could not allocate enough memory for frames");
 
+	if (oneshot) {
+		run(camera, ab, debug);
+		goto end;
+	}
+
 	while (!IS_STOPPED()) {
-		if (uvc_capture_frame(camera)) {
-			if (!appbase_push_frame(ab, camera->frame, camera->frame_size))
-				fprintf(stderr, "ERROR: Could not send frame\n");
+		run(camera, ab, debug);
 
-			memset(camera->frame, 0, camera->frame_size);
-		}
-
-		/* sleep(3) should not interfere with our signal handlers, unless we're also handling SIGALRM */
+		/*
+		 * sleep(3) should not interfere with our signal handlers,
+		 * unless we're also handling SIGALRM
+		 */
 		sleep(wait_time);
 	}
 
+end:
 	uvc_close(camera);
 	appbase_close(ab);
 
